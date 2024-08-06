d90907a8c2082fd01f5bb2bd7895b357065d47e2
root <root@inst-builder-debian-12-build-build-zz6hj.us-central1-a.c.gce-image-builder.internal>
2024-08-06 23:01:24 +0000

    Updated  S3 bucket value in provider.tf file

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ terraform {
   required_version = ">= 0.14.0"
 
   backend "s3" {
-    bucket = "ase-terraform-state-bucket"
+    bucket = "f7ffc7d4-fa14-411e-ae2d-56c5b8555b51"
     key    = "terraform.tfstate"
     region = "us-west-2"
   }
